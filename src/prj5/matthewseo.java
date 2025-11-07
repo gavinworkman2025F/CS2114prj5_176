a470c0d3cf078be18a50ba9a412d78acb284b41b
Matthew Seo <mjseo@vt.edu>
2025-11-07 14:07:47 -0500

    Update matthewseo.java

--- src/prj5/matthewseo.java
+++ src/prj5/matthewseo.java
@@ -1,11 +1,12 @@
 package prj5;
+
 public class matthewseo
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
         System.out.println("Glad to be on your team");
 
     }
-
 
 }
