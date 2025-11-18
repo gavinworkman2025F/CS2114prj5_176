e0140b2593822c4768dfe213006ed2c91d9c85fb
gavin <gjworkman07@protonmail.com>
2025-11-18 12:55:30 -0500

    Added Node Class

--- src/prj5/Node.java
+++ src/prj5/Node.java
@@ -1,15 +1,34 @@
 package prj5;
 public class Node<T>
 {
-
+    private T data;
+    private Node<T> next;
+    
     public Node()
     {
-        // TODO Auto-generated constructor stub
+        data = null;
+        next = null;
     }
-    //~ Fields ................................................................
-
-    //~ Constructors ..........................................................
-
-    //~Public  Methods ........................................................
+    
+    public Node(T d, Node<T> n) {
+        data = d;
+        next = n;
+    }
+    
+    public Node<T> getNext() {
+        return next;
+    }
+    
+    public void setNext(Node<T> n) {
+        next = n;
+    }
+    
+    public T getData() {
+        return data;
+    }
+    
+    public void setData(T d) {
+        data = d;
+    }
 
 }
