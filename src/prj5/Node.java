7ae2ebcec4dbbb88c5f523c1ca25a243a9ea4f3c
gavin <gjworkman07@protonmail.com>
2025-11-18 12:48:07 -0500

    added node class

---
+++ src/prj5/Node.java
@@ -0,0 +1,15 @@
+package prj5;
+public class Node<T>
+{
+
+    public Node()
+    {
+        // TODO Auto-generated constructor stub
+    }
+    //~ Fields ................................................................
+
+    //~ Constructors ..........................................................
+
+    //~Public  Methods ........................................................
+
+}
